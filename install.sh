2b0bbc460b3fc5538bcfeefa5fb96f4575c559bc
w4zu <adico_kd@live.fr>
2020-03-17 16:30:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ website=jisti.youdomain.com # use the same during installation of jisti
 echo 'deb https://download.jitsi.org stable/' >> /etc/apt/sources.list.d/jitsi-stable.list
 wget -qO -  https://download.jitsi.org/jitsi-key.gpg.key | apt-key add -
 apt-get update
-echo "During the installation, you will be asked to enter the hostname of the Jitsi Meet instance. If you have a FQDN hostname for the instance already set up in DNS, enter it there. If you don't have a resolvable hostname, you can enter the IP address of the machine (if it is static or doesn't change)."
+echo "During the installation, you will be asked to enter the hostname of the Jitsi Meet instance."
 echo "This hostname will be used for virtualhost configuration inside the Jitsi Meet and also, you and your correspondents will be using it to access the web conferences."
 sleep 10
 apt-get -y install jitsi-meet
